8d268865a37db8c63b43f5909793e54de8a656f0
Santiago Marranti <marrantis@gmail.com>
2025-06-04 20:25:38 -0300

    Agrego nueva funcionalidad "multiplicacion" y corrijo algunos errores en los prints

--- multiplicacion.py
+++ multiplicacion.py
@@ -1,4 +1,4 @@
 a = int(input("Ingrese un numero"));
 b = int(input("Ingrese otro numero"));
 
-print("La division es: " + (a / b));
+print("La multiplicacion es: " + (a * b));
--- suma.py
+++ suma.py
@@ -1,4 +1,4 @@
 a = int(input("Ingrese un numero"));
 b = int(input("Ingrese otro numero"));
 
-print("La resta es: " + (a + b));
+print("La suma es: " + (a + b));
